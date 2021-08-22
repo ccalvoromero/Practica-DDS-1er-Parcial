3a47ca4ffc9e3214f095b87c42ef1cb381095e7e
Cristopher Calvo Romero <ccromero@veritran.com>
2021-08-22 18:06:32 -0300

    refactor: details

--- src/main/java/net/dds/usecase/BuyMovie.java
+++ src/main/java/net/dds/usecase/BuyMovie.java
@@ -1,5 +1,29 @@
 package net.dds.usecase;
 
+import net.dds.domain.movie.Movie;
+import net.dds.domain.MovieRepository;
+import net.dds.domain.customer.Customer;
+import net.dds.domain.CustomerRepository;
+import net.dds.domain.payment.PaymentMethod;
+
 public class BuyMovie {
+
+    private final PaymentMethod paymentMethod;
+    private final MovieRepository movieRepository;
+    private final CustomerRepository customerRepository;
+
+    public BuyMovie(PaymentMethod paymentMethod, MovieRepository movieRepository, CustomerRepository customerRepository) {
+        this.paymentMethod = paymentMethod;
+        this.movieRepository = movieRepository;
+        this.customerRepository = customerRepository;
+    }
+
+    public void execute(Integer movieId, Integer customerDocumentNumber) {
+        Movie movie = movieRepository.findAvailableMovie(movieId);
+        Customer customer = customerRepository.findByDocumentNumber(customerDocumentNumber);
+        customer.buyMovie(movie, paymentMethod);
+        movieRepository.save(movie);
+        customerRepository.save(customer);
+    }
 
 }
